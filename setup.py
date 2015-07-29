e1855ec1f9df59ba2ee9d7c4c4a3f2b3d4edac55
viktor <viktor@rutarget.ru>
2015-07-29 16:56:49 +0300

    init commit

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ if os.environ.get('READTHEDOCS', None) == 'True':
 
 setup(
     name='luigi',
-    version='1.2.2-rutarget',
+    version='1.2.2-r',
     description='Workflow mgmgt + task scheduling + dependency resolution',
     long_description=long_description,
     author='Erik Bernhardsson',
